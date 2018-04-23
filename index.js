b68a883dd111cfcf506dc271bae0a784b326a301
Asrul Sani Ariesandy <asrul@kata.ai>
2018-04-23 07:47:28 +0700

    updated test

--- index.js
+++ index.js
@@ -5,61 +5,23 @@ const bodyParser = require("body-parser");
 const router = require("./lib/router");
 const getfn = require("./lib/getfn");
 
-module.exports = function (merapi) {
+module.exports = function () {
 
     return {
-        apps: [],
 
-        typeExpress(name, opt) {
+        *onBeforeComponentsRegister(container) {
+            container.register("MerapiWebsocket", require("./lib/merapi_websocket"));
+        },
+        typeWebsocket(name, opt) {
             this.apps.push(name);
             return function* (config, injector, logger) {
-                let app = express();
-
-                let getFn = getfn(injector);
-
-                opt.config = opt.config || "app";
-                let cfg = config.default(opt.config, {});
-
-                let port = cfg.port || 8080;
-                let host = cfg.host || "localhost";
-                let routerOptions = Object.assign({}, { mergeParams: true }, cfg.router);
-                let bodyParserOptions = cfg.bodyParser || {};
-                let middleware = cfg.middleware || [];
-                let routes = cfg.routes || {};
-
-                if (bodyParserOptions.verify) {
-                    bodyParserOptions.verify = yield getFn(bodyParserOptions.verify);
-                }
-
-                app.use(bodyParser.json(bodyParserOptions));
-                app.use(bodyParser.urlencoded({ extended: true }));
-
-                let isRoutesInMiddleware = false;
-
-                for (let i = 0; i < middleware.length; i++) {
-                    if (middleware[i] === 'routes') {
-                        app.use(yield router(injector, routes, routerOptions));
-                        isRoutesInMiddleware = !isRoutesInMiddleware;
-                    } else {
-                        app.use(yield getFn(middleware[i]));
-                    }
-                }
-
-                if (!isRoutesInMiddleware) app.use(yield router(injector, routes, routerOptions));
-
-                app.start = function () {
-                    app.listen(port, host);
-                    logger.info(`Starting express on ${host}:${port}`);
-                };
+                
                 return app;
             };
         },
-
-        *onStart() {
-            for (let i = 0; i < this.apps.length; i++) {
-                let app = yield merapi.resolve(this.apps[i]);
-                app.start();
-            }
-        }
+        *onStart(container) {
+            let socket = yield container.resolve("MerapiWebsocket");
+            yield service.start();
+        },
     };
 };
