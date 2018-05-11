d8a65e83063f4be050ca829a42f6f7a10a5bdfc8
Asrul Sani Ariesandy <asrul@kata.ai>
2018-05-11 16:24:31 +0700

    Fixed: bugs

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 "use strict";
 
-const express = require("express");
-const bodyParser = require("body-parser");
 const WebsocketManager = require("./lib/websocket_manager");
 
 module.exports = function () {
 
     return {
 
-        *onBeforeComponentsRegister(container) {
-            container.register("websocketManager", WebsocketManager);
+        *onAfterComponentsRegister(container) {
+            container.register("websocket", WebsocketManager);
         },
-        typeWebsocket(name, opt) {
-            // this.apps.push(name);
-            return function* (websocketManager, config) {
-                opt.config = opt.config || "websocket";
-                let cfg = config.default(opt.config, {});
 
-                return websocketManager.initialize(cfg);
-            };
-        },
+        *onStart(container) {
+            const websocket = yield container.resolve("websocket");
+        }
     };
 };
